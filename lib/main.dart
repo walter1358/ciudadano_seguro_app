1af28732535537972c0fe009068f11da7e6a9d32
Walter Collantes <wcmore.93@gmail.com>
2025-11-29 01:54:07 -0500

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // 👈 importante
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/home_screen.dart';
@@ -12,7 +12,7 @@ import 'services/auth_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  await dotenv.load(fileName: "assets/.env"); // 👈 carga la API KEY
+  await dotenv.load(fileName: "assets/.env");
   runApp(const CiudadanoSeguroApp());
 }
 
